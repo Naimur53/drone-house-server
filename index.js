290e9c5edb4915c22f61a283ad221aedd4917b1b
Naimur53 <naimurrhman53@gmail.com>
2021-11-13 21:08:55 +0600

    done

--- index.js
+++ index.js
@@ -7,9 +7,13 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 
 const port = process.env.PORT || 5000;
+const corsOptions = {
+    origin: '*',
+    credentials: true,            //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+}
 
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
